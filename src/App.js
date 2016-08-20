5aafe8a01e2a32f14c68d98faa37959d0f65b699
John Dutamby <jdutamby@alkemics.com>
2016-08-21 01:45:25 +0200

    added input and sync with database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar.js';
 import * as firebase from "firebase";
 
 class App extends Component {
@@ -22,15 +23,23 @@ class App extends Component {
     });
   }
 
+  handleTermChange(term) {
+    const rootRef = firebase.database().ref().child('react');
+    const wordRef = rootRef.child('word');
+    wordRef.set(term)
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+
         </div>
 
           <h1>{this.state.speed}</h1>
+                    <SearchBar onTermChange={this.handleTermChange}/>
 
       </div>
     );
